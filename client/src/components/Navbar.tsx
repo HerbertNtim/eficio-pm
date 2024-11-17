260984c216087fbe12b97d0603875a9eac4134cf
Herbert <herbetntim@gmail.com>
2024-11-17 13:09:55 +0000

    added menu for openning sidebar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,10 +1,21 @@
-import { Search, Settings } from 'lucide-react'
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsSidebarCollapsed } from '@/state'
+import { Menu, Search, Settings } from 'lucide-react'
 import Link from 'next/link'
 const Navbar = () => {
+  const dispatch = useAppDispatch()
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed)
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode)
+
   return (
     <div className='flex items-center justify-between bg-white px-4 py-3 dark:bg-black'>
       {/* Search Bar */}
       <div className='flex items-center gap-8'>
+        {!isSidebarCollapsed ? null : (
+          <button onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}>
+          <Menu className='h-8 w-8 dark:text-white' /> 
+        </button>
+        )}
         <div className='relative w-[200px] h-min flex'>
           <Search className='absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white'/>
           <input type="search" placeholder='Search...' className='input' />
