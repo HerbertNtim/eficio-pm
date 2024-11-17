b638e7fa75551c597df41c4ec00eb4c96a0c3ac4
Herbert <herbetntim@gmail.com>
2024-11-17 01:42:52 +0000

    implemented api skeleton

---
+++ client/src/state/api.ts
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+  }),
+  endpoints: (build) => ({
+    // Define your endpoints here
+  }),
+});
+
+export const {} = api
