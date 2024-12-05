4805389fc5ab15759c3ec6b52afea0b8d8c31ca1
Herbert <herbetntim@gmail.com>
2024-12-05 17:14:56 +0000

    added buttons

--- client/src/app/project/[id]/page.tsx
+++ client/src/app/project/[id]/page.tsx
@@ -1,16 +1,21 @@
+"use client"
+
 import React, { useState } from 'react'
+import ProjectHeader from '../ProjectHeader'
 
 type Props = {
   params: {id: string}
 }
 
 function Project({params}: Props) {
+  // const {id} = params
+
   const [activeTab, setActiveTab] = React.useState('Board')
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false)
   
   return (
     <div>
-
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
     </div>
   )
 }
