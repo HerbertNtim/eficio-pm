81e08891806e2d9a1a5570d7049a0cdb3d7567a1
Herbert <herbetntim@gmail.com>
2024-12-12 16:00:02 +0000

    implemented api from the frontend to the backend

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -71,7 +71,7 @@ export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
   }),
-  tagTypes: ["Projects", "Tasks"],
+  tagTypes: ["Projects", "Tasks", "Users"],
   endpoints: (build) => ({
     getProjects: build.query<Project[], void>({
       query: () => "projects",
@@ -107,10 +107,14 @@ export const api = createApi({
         { type: "Tasks", id: taskId },
       ],
     }),
+    getUsers: build.query<User[], void>({
+      query: () => "users",
+      providesTags: ["Users"],
+    }),
     search: build.query<SearchResults, string>({
       query: (query) => `search?query=${query}`
     })
   }),
 });
 
-export const { useCreateProjectMutation, useGetProjectsQuery, useGetTaskQuery, useCreateTaskMutation, useUpdateTaskStatusMutation, useSearchQuery } = api;
+export const { useCreateProjectMutation, useGetProjectsQuery, useGetTaskQuery, useCreateTaskMutation, useUpdateTaskStatusMutation, useSearchQuery, useGetUsersQuery } = api;
