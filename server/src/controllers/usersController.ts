5b284e48fb56040064f948a654d5863689b9498e
Herbert <herbetntim@gmail.com>
2024-12-12 16:29:48 +0000

    implemented team controller

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -5,8 +5,28 @@ const prisma = new PrismaClient();
 
 export const getUsers = async (req: Request, res: Response): Promise<void> => {
   try {
-    const users = await prisma.user.findMany()
-    res.status(200).json(users);
+    const teams = await prisma.team.findMany()
+    const teamsWithUsernames = await Promise.all(
+      teams.map(async (team: any) => {
+        const projectOwner = await prisma.user.findUnique({
+          where: { userId: team.projectOwnerId! },
+          select: { username: true },
+        });
+        
+        const projectManager = await prisma.user.findUnique({
+          where: { userId: team.projectManagerId! },
+          select: { username: true },
+        })
+        
+        return {
+          ...team,
+          projectOwnerUsername: projectOwner?.username,
+          projectManagerUsername: projectManager?.username
+        }
+      }) 
+    )
+
+    res.status(200).json();
   } catch (error: any) {
     res.status(500).json({ message: `Error getting users: ${error.message}` });
   }
