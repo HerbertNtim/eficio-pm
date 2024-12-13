7145861bbe3132960e579c885e2bbfc8e9f7b879
Herbert <herbetntim@gmail.com>
2024-12-13 15:52:09 +0000

    wrapped up

--- client/src/app/priority/reusablePriorityPage/index.tsx
+++ client/src/app/priority/reusablePriorityPage/index.tsx
@@ -62,13 +62,13 @@ const columns: GridColDef[] = [
     field: "author",
     headerName: "Author",
     width: 150,
-    renderCell: (params) => params.value.username || "Unknown",
+    renderCell: (params) => params.value?.username || "Unknown",
   },
   {
     field: "assignee",
     headerName: "Assignee",
     width: 150,
-    renderCell: (params) => params.value.username || "Unassigned",
+    renderCell: (params) => params.value?.username || "Unassigned",
   },
 ];
 
@@ -76,13 +76,12 @@ const ReusablePriorityPage = ({ priority }: Props) => {
   const [view, setView] = useState("list");
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
 
-  const { data: currentUser } = useGetAuthUserQuery({});
-  const userId = currentUser?.userDetails?.userId ?? null;
-  const {
+  const userId = 1;
+   const {
     data: tasks,
     isLoading,
     isError: isTasksError,
-  } = useGetTasksByUserQuery(userId || 0, {
+  } = useGetTasksByUserQuery({ userId: userId || 0 }, {
     skip: userId === null,
   });
 
