8a7f0895aeb0e5c1eec13723e377a6ffb3851c0c
Herbert <herbetntim@gmail.com>
2024-12-15 14:52:13 +0000

    updated project page

--- client/src/app/project/[id]/page.tsx
+++ client/src/app/project/[id]/page.tsx
@@ -9,7 +9,7 @@ export default async function ProjectPage({ params }: Props) {
   const { id } = await params; 
 
   if(!id) {
-    return <div>Project not found</div>
+    return <div className="p-8 dark:bg-dark-secondary dark:text-white w-full h-screen text-center ">Project not found</div>
   }
   
   return <Project id={id} />;
