4aa51d83dc1e281e18af2181064807129a271044
Herbert <herbetntim@gmail.com>
2024-12-18 00:56:20 +0000

    added server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -35,7 +35,7 @@ app.use("/users", usersRouter);
 app.use("/teams", teamRouter);
 
 // SERVER
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 5000
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`)
 })
